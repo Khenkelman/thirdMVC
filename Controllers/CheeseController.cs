413e56429ead8cd33e5ed3b3479d6b9cb6c03716
Khenkelman <61330161+Khenkelman@users.noreply.github.com>
2022-04-28 11:37:17 -0500

    Initial Commit

---
+++ Controllers/CheeseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private List<string> Cheeses = new List<string>();
+        public IActionResult Index()
+        {
+
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+
+        }
+        public IActionResult Remove()
+        {
+
+            ViewBag.cheeses = Cheeses;
+            return View();
+
+        }
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            Cheeses.Add(name);
+            return Redirect("/Cheese");
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Remove")]
+        public IActionResult RemoveCheese(string name)
+        {
+            Cheeses.Remove(name);
+            return Redirect("/Cheese");
+        }
+    }
+}
